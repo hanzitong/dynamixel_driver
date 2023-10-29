d783d3a298b9a9c5970f2ce4581757eb7b79e156
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2023-10-29 10:24:09 +0900

    add param and yaml

--- src/dxl_x_driver.cpp
+++ src/dxl_x_driver.cpp
@@ -4,52 +4,12 @@
 #include <chrono>
 #include <functional>
 #include <string>
-
 #include <rclcpp/rclcpp.hpp>
-#include <dynamixel_sdk/dynamixel_sdk.h>
-/* MEMO: where to install dynamixel_sdk by default Makefile
-        library: /usr/local/lib
-        header: /usr/local/include
-*/
-
 #include <geometry_msgs/msg/twist.hpp>
-
+#include <dynamixel_sdk/dynamixel_sdk.h>
+#include <dynamixel_driver/control_table.hpp>
 using namespace std::chrono_literals;
-
-/* ===== basic settings ===== */
-#define PROTOCOL_VERSION        2.0
-#define DXL_ID                  1
-#define BAUDRATE                9600
-#define DEVICENAME              "/dev/ttyUSB0"
-
-/* ===== control table address ===== */
-#define ADDR_OPERATING_MODE       11
-#define ADDR_CURRENT_LIMIT        38
-#define ADDR_VELOCITY_LIMIT       44
-// #define ADDR_MAX_POSITION_LIMIT   48
-// #define ADDR_MIN_POSITION_LIMIT   52
-#define ADDR_TORQUE_ENABLE        64
-#define ADDR_VELOCITY_I_GAIN      76
-#define ADDR_VELOCITY_P_GAIN      78
-// #define ADDR_POSITION_D_GAIN      80
-// #define ADDR_POSITION_I_GAIN      82
-// #define ADDR_POSITION_P_GAIN      84
-#define ADDR_GOAL_VELOCITY        104
-// #define ADDR_GOAL_POSITION          116
-#define ADDR_PRESENT_CURRENT        126
-#define ADDR_PRESENT_VELOCITY       128
-// #define ADDR_PRESENT_POSITION       132
-
-/* ===== control table value ===== */
-#define VELOCITY_CONTROL_MODE   1
-// #define POSITION_CONTROL_MODE   3
-#define TORQUE_ENABLE           1
-#define TORQUE_DISABLE          0
-#define VELOCITY_LIMIT          210
-// #define MAXIMUM_POSITION_LIMIT  4095
-// #define MINIMUM_POSITION_LIMIT  0
-#define DXL_MOVING_STATUS_THRESHOLD 20
-
+using std::placeholders::_1;
 
 class DxlXDriver : public rclcpp::Node
 {
@@ -63,57 +23,28 @@ public:
         INSTRUCTION_DISACTIVATE,
     };
 
-    DxlXDriver(): Node("dxl_x_driver_node")
+    DxlXDriver(): 
+        Node("dxl_x_driver_node"), 
+        dxl_instruction_(Dxl_instruction::INSTRUCTION_INITIAL), 
+        dxl_is_ready_(false),
+        dxl_error_(0),
+        goal_velocity_(VELOCITY_LIMIT-20)
     {
-        this -> declare_parameter("dxl_id", DXL_ID);
-        this -> declare_parameter("baudrate", BAUDRATE);
-        this -> declare_parameter("device_file", DEVICENAME);
+        this -> declare_parameter("dxl_id", 1);
+        this -> declare_parameter("baudrate", 9600);
+        this -> declare_parameter("device_file", "/dev/ttyUSB0");
+        dxl_id_ = this -> get_parameter("dxl_id").as_int();
+        baudrate_ = this -> get_parameter("baudrate").as_int();
+        device_file_ = this -> get_parameter("device_file").as_string().c_str();
 
-        dxl_instruction_ = Dxl_instruction::INSTRUCTION_INITIAL;
-        dxl_is_ready_ = false;
-        dxl_error_ = 0;
-        portHandler_ = dynamixel::PortHandler::getPortHandler(DEVICENAME);
+        portHandler_ = dynamixel::PortHandler::getPortHandler(device_file_);
         packetHandler_ = dynamixel::PacketHandler::getPacketHandler(PROTOCOL_VERSION);
+        publisher_ = this -> create_publisher<geometry_msgs::msg::Twist>("dxl_present_velocity", 10);
+        timer_ = this -> create_wall_timer(500ms, std::bind(&DxlXDriver::timer_callback, this));
+        subscription_ = this -> create_subscription<geometry_msgs::msg::Twist>(
+            "dxl_cmd_vel", 10, std::bind(&DxlXDriver::subscription_callback, this, _1));
 
-        timer_ = this -> create_wall_timer(500ms, std::bind(&DxlXDriver::callback, this));
-        // publisher_ = this -> create_publisher<std_msg::aaaaaa>("dxl_present_velocity", 10);
-        // subscription_ = this -> create_subscription<geometry_msgs::twist>(
-        //     "dxl_goal_velocity", 10, std::bind(DxlXDriver::callback, this, _1));
-
-        /* open port */
-        if (portHandler_ -> openPort()){
-            RCLCPP_INFO(this -> get_logger(), "succeed to open port");
-        } else {
-            RCLCPP_ERROR(this -> get_logger(), "fail to open port");
-        }
-
-        /* set baudrate */
-        if (portHandler_ -> setBaudRate(BAUDRATE)) {
-            RCLCPP_INFO(this -> get_logger(), "succeed to set baudrate");
-        } else {
-            RCLCPP_ERROR(this -> get_logger(), "failed to set baudrate");
-        } 
-
-        /* set operating mode */
-        int dxl_comm_result = packetHandler_ -> write1ByteTxRx(
-            portHandler_, DXL_ID, ADDR_OPERATING_MODE, VELOCITY_CONTROL_MODE, &dxl_error_);
-        if (dxl_comm_result != COMM_SUCCESS || dxl_error_ != 0) {
-            RCLCPP_ERROR(this -> get_logger(), "failed to set operating mode");
-        } else {
-            RCLCPP_INFO(this -> get_logger(), "Operating mode: velocity control mode.");
-        }
-
-        /* set velocity limit */
-        dxl_comm_result = packetHandler_ -> write4ByteTxRx(
-            portHandler_, DXL_ID, ADDR_VELOCITY_LIMIT, VELOCITY_LIMIT, &dxl_error_);
-        if (dxl_comm_result != 0 || dxl_error_ != 0) {
-            RCLCPP_ERROR(this -> get_logger(), "failed to set velocity limit");
-        } else {
-            RCLCPP_INFO(this -> get_logger(), "succeed to set velocity limit");
-        }
-
-        /* need further implementation */
-        dxl_instruction_ = Dxl_instruction::INSTRUCTION_ACTIVATE;
+        // stand_up();
     }
 
     ~DxlXDriver() {
@@ -122,19 +53,16 @@ public:
     }
 
 private:
-    void callback()
+    void timer_callback()
     {
-        // geometry_msgs::msg::twist msg;
-        // RCLCPP_INFO(this -> get_logger(), "id:%s, baudrate:%s, port:%s, mode:%s, limit:%s", my_param.c_str());
-
         /* check instruction */
         switch (dxl_instruction_) {
             case Dxl_instruction::INSTRUCTION_ACTIVATE:
                 step_activate();
-                dxl_instruction_ = Dxl_instruction::INSTRUCTION_RUN_VELOCITY;
                 // break;
             case Dxl_instruction::INSTRUCTION_RUN_VELOCITY:
-                step_run_velocity();
+                dxl_instruction_ = Dxl_instruction::INSTRUCTION_RUN_VELOCITY;
+                step_run_velocity(goal_velocity_);
                 break;
             case Dxl_instruction::INSTRUCTION_STOP:
                 step_zero_velocity();
@@ -147,98 +75,171 @@ private:
                 break;
         }
 
-        step_read_velocity();
+        geometry_msgs::msg::Twist msg;
+        msg.linear.x = step_read_velocity();
+        msg.linear.y = step_read_present_current();
 
-        // publisher_ -> publish(msg);
+        publisher_ -> publish(msg);
     }
 
-    void step_activate(){
+    void subscription_callback(geometry_msgs::msg::Twist msg){
+        /* set goal velocity */
+        // goal_velocity_ = msg.linear.y;   // example
+        goal_velocity_ = VELOCITY_LIMIT - 20;
+
+        return;
+    }
+
+    void stand_up(){
+        /* open port */
+        if (portHandler_ -> openPort()){
+            RCLCPP_INFO(this -> get_logger(), "succeed to open port");
+        } else {
+            RCLCPP_ERROR(this -> get_logger(), "fail to open port");
+        }
+
+        /* set baudrate */
+        if (portHandler_ -> setBaudRate(baudrate_)) {
+            RCLCPP_INFO(this -> get_logger(), "succeed to set baudrate");
+        } else {
+            RCLCPP_ERROR(this -> get_logger(), "failed to set baudrate");
+        }
+
+        /* set operating mode */
+        int dxl_comm_result = packetHandler_ -> write1ByteTxRx(
+            portHandler_, dxl_id_, ADDR_OPERATING_MODE, VELOCITY_CONTROL_MODE, &dxl_error_);
+        if (dxl_comm_result != COMM_SUCCESS || dxl_error_ != 0) {
+            RCLCPP_ERROR(this -> get_logger(), "failed to set operating mode");
+        } else {
+            RCLCPP_INFO(this -> get_logger(), "Operating mode: velocity control mode.");
+        }
+
+        /* set velocity limit */
+        dxl_comm_result = packetHandler_ -> write4ByteTxRx(
+            portHandler_, dxl_id_, ADDR_VELOCITY_LIMIT, VELOCITY_LIMIT, &dxl_error_);
+        if (dxl_comm_result != 0 || dxl_error_ != 0) {
+            RCLCPP_ERROR(this -> get_logger(), "failed to set velocity limit");
+        } else {
+            RCLCPP_INFO(this -> get_logger(), "succeed to set velocity limit");
+        }
+
+        /* need further implementation */
+        dxl_instruction_ = Dxl_instruction::INSTRUCTION_ACTIVATE;
+    }
+
+    bool step_activate(){
         if (dxl_is_ready_) {
             RCLCPP_INFO(this -> get_logger(), "dxl is already ready");
+            return true;
         } else {
             /* enable torque */
             int dxl_comm_result = packetHandler_ -> write1ByteTxRx(
-                portHandler_, DXL_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE, &dxl_error_);
+                portHandler_, dxl_id_, ADDR_TORQUE_ENABLE, TORQUE_ENABLE, &dxl_error_);
             if (dxl_comm_result != 0 || dxl_error_ != 0){
                 RCLCPP_ERROR(this -> get_logger(), "failed to enable dxl torque");
                 dxl_is_ready_ = false;
+                return false;
             } else {
                 dxl_is_ready_ = true;
                 RCLCPP_INFO(this -> get_logger(), "dxl: activate");
+                return true;
             }   
         }
     }
 
-    void step_run_velocity(){
+    bool step_run_velocity(double goal_velocity){
         if (dxl_is_ready_) {
             /* set motor goal velocity */
             int dxl_comm_result = packetHandler_ -> write4ByteTxRx(
-                portHandler_, DXL_ID, ADDR_GOAL_VELOCITY, VELOCITY_LIMIT-20, &dxl_error_);
+                portHandler_, dxl_id_, ADDR_GOAL_VELOCITY, goal_velocity, &dxl_error_);
             if (dxl_comm_result != 0 || dxl_error_ != 0){
                 RCLCPP_ERROR(this -> get_logger(), "failed to set goal velocity");
+                return false;
             }
+            return true;
         } else {
             RCLCPP_ERROR(this -> get_logger(), "dxl is not ready");
+            return false;
         }
     }
 
-    void step_zero_velocity(){
+    bool step_zero_velocity(){
         if (dxl_is_ready_) {
             /* set motor velocity zero */
             int dxl_comm_result = packetHandler_ -> write4ByteTxRx(
-                portHandler_, DXL_ID, ADDR_GOAL_VELOCITY, 0, &dxl_error_);
+                portHandler_, dxl_id_, ADDR_GOAL_VELOCITY, 0, &dxl_error_);
             if (dxl_comm_result != 0 || dxl_error_ != 0){
                 RCLCPP_ERROR(this -> get_logger(), "failed to set zero velocity");
+                return false;
             } else {
                 RCLCPP_INFO(this -> get_logger(), "set zero velocity");
+                return true;
             }
         } else {
             RCLCPP_ERROR(this -> get_logger(), "dxl is not ready");
+            return false;
         }
     }
 
-    void step_read_velocity(){
+    double step_read_velocity(){
         /* read motor velocity */
-        uint16_t dxl_present_velocity = 0;
+        uint32_t dxl_present_velocity = 0;
         int dxl_comm_result = packetHandler_ -> read4ByteTxRx(
-            portHandler_, DXL_ID, ADDR_PRESENT_VELOCITY, (uint32_t*)&dxl_present_velocity, &dxl_error_); 
+            portHandler_, dxl_id_, ADDR_PRESENT_VELOCITY, (uint32_t*)&dxl_present_velocity, &dxl_error_); 
         if (dxl_comm_result != 0 || dxl_error_ != 0) {
             RCLCPP_ERROR(this -> get_logger(), "failed to read present velocity");
+            return 0.;  // false
         } else {
             RCLCPP_INFO(this -> get_logger(), "present velocity: %u[rpm]", dxl_present_velocity);
+            return (double)dxl_present_velocity;
         }
     }
 
-    void step_disactivate(){
+    double step_read_present_current(){ // changed type 
+        /* read present current */
+        uint16_t dxl_present_current = 0;
+        int dxl_comm_result = packetHandler_ -> read2ByteTxRx(
+            portHandler_, dxl_id_, ADDR_PRESENT_CURRENT, (uint16_t*)&dxl_present_current, &dxl_error_); 
+        if (dxl_comm_result != 0 || dxl_error_ != 0) {
+            RCLCPP_ERROR(this -> get_logger(), "failed to read present current");
+            return 0.;  // false
+        } else {
+            RCLCPP_INFO(this -> get_logger(), "present current: %u[mA]", dxl_present_current);
+            return (double)dxl_present_current;
+        }
+    }
+
+    bool step_disactivate(){
         if (dxl_is_ready_) {
             /* disable torque*/
             int dxl_comm_result = packetHandler_ -> write1ByteTxRx(
-                portHandler_, DXL_ID, ADDR_TORQUE_ENABLE, TORQUE_DISABLE, &dxl_error_);
+                portHandler_, dxl_id_, ADDR_TORQUE_ENABLE, TORQUE_DISABLE, &dxl_error_);
             if (dxl_comm_result != 0 || dxl_error_ != 0){
                 RCLCPP_ERROR(this -> get_logger(), "failed to enable dxl torque");
+                return false;
             } else {
                 RCLCPP_INFO(this -> get_logger(), "dxl: disactivate");
                 dxl_is_ready_ = false;
+                return true;
             }
         } else {
             RCLCPP_ERROR(this -> get_logger(), "dxl is already torque off");
+            return true;
         }
     }
 
-    // void change_instruction(){
-    //     dxl_instruction_ = ;
-    // }
-
-    rclcpp::TimerBase::SharedPtr timer_;
-    // rclcpp::Publisher<geometry_msgs::msg::twist>::SharedPtr publisher_;
-
-    uint8_t dxl_error_;
-    // uint8_t dxl_instruction_;
+    int dxl_id_;
+    int baudrate_;
+    const char* device_file_;
     Dxl_instruction dxl_instruction_;
     bool dxl_is_ready_;
-    int velocity_value;
+    uint8_t dxl_error_;
+    double goal_velocity_;
     dynamixel::PortHandler *portHandler_;
     dynamixel::PacketHandler *packetHandler_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscription_;
 };
 
 
