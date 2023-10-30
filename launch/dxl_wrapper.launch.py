47235e4902f66f8355f82261b0e0db594150ea07
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2023-10-31 07:18:20 +0900

    add launch dir and dxl_wrapper.launch.py

---
+++ launch/dxl_wrapper.launch.py
@@ -0,0 +1,29 @@
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory("dynamixel_driver"),
+        "config",
+        "dxl_x_driver.yaml"
+    )
+
+    return LaunchDescription([
+        Node(
+            package="dynamixel_driver",
+            executable="dxl_x_driver",
+            parameters=[config],
+            namespace="dxl1",
+            name="driver"
+        )
+    ])
+
+
+
+
+
